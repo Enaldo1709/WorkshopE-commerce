c15b8ecb3e07b3721616d9640b26c99d93c0a754
Andres Rojas <carlos.rojas4538gmail.com>
2021-05-11 19:37:42 -0500

    Se agrega code.js con funcion y contructor

--- code.js
+++ code.js
@@ -1,2 +1,27 @@
 "use strict"
 
+
+
+class product{
+
+    constructor(name, price, url_img,id){
+
+        
+        this.name = name
+        this.price =price
+        this.url_img= url_img
+        this.id=id
+        
+    }
+
+    add_product(Products_list){
+        Products_list.push(this)
+    }
+}
+
+
+const pantalon = new product("pantalon",1000,"https://www.instyle.es/medio/2019/02/04/pantalones-vaqueros-cropped-primavera-uterque_b2a5edab_1000x1499.jpg",1)
+
+// pregnuntar por que no se muestra en pantalla: console.log(pantalon)
+const buy_list=[]
+pantalon.add_product()
